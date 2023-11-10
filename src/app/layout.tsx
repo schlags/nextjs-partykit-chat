aefcf6199b98cb1a3ac5a92b78623646e162af92
Dylan Schlager <mail@dylanschlager.com>
2023-11-09 22:40:43 -0700

    change title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Play Tic Tac Toe',
+  title: 'Chat | DylanSchlager.com',
   description: 'Powered by PartyKit!',
 }
 
