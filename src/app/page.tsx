ea5fc07da6320b8ee06ff07a47830cb5924563d6
Dylan Schlager <mail@dylanschlager.com>
2023-11-09 21:58:36 -0700

    feat(chat): first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,175 @@
-import Image from 'next/image'
+'use client';
+
+
+import React from "react";
+import usePartySocket from "partysocket/react";
+import { useState, useEffect, useRef } from "react";
+import uuid from "react-uuid";
+
+interface ChatMessage {
+  user: string;
+  message: string;
+  time: Date;
+}
+
+interface ConnectionStatus {
+  status: string;
+  userName?: string;
+}
+
+function Message({ message }: { key: string, message: ChatMessage }) {
+  const userAvatar = `https://api.dicebear.com/7.x/pixel-art/svg?seed=${message.user}&background=%23fff&radius=50&margin=10`
+  return (
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+    <div className="shrink-0">
+      <img className="h-12 w-12" src={userAvatar} alt="User's avatar" />
+    </div>
+      <div>
+        <div className="text-xs font-medium text-slate-500">{message.user}</div>
+        <p className="text-black">{message.message}</p>
+      </div>
+      {/* time stamp */}
+      <div className="text-xs font-small text-slate-200">{message.time.toLocaleTimeString()}</div>
+    </div>
+  )
+}
+
+function DisplayMessages( { messages }: { messages: ChatMessage[] } ) {
+  const messagesEndRef = useRef<null | HTMLDivElement>(null)
+
+  const scrollToBottom = () => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" })
+    }
+  }
+
+  useEffect(() => {scrollToBottom()}, [messages]);
+  return (
+    <div>
+      {messages.map(message => <Message key={uuid()} message={message} />)}
+      <div ref={messagesEndRef} />
+    </div>
+  );
+}
+
+function DisplayServerMessages( { messages }: { messages: string[] } ) {
+  return (
+    messages.map((message) => {
+      return (
+        <div key={uuid()} className="bg-gray-100 rounded-lg p-2">
+          <p className="text-gray-800">{message}</p>
+        </div>
+      );
+    })
+  );
+
+}
+
+function ConnectionPing() {
+  return (
+    <span className="relative flex h-3 w-3 top-0 right-0">
+      <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+      <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+  </span>
+  )
+}
+
+function ShowConnectionStatus( { status }: { status: ConnectionStatus } ) {
+  return (
+    <span className="relative inline-flex justify-center">
+      <span className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-sky-500 bg-white dark:bg-slate-800 transition ease-in-out cursor-default duration-150 ring-1 ring-slate-900/10 dark:ring-slate-200/20">
+        {status.status} {status.userName ? `as ${status.userName}` : null}
+      </span>
+      {status.status === "Connected" ? <ConnectionPing /> : null}
+    </span>
+  )
+
+}
 
 export default function Home() {
+  const [chatMessages, setChatMessages] = useState<ChatMessage[]>([]);
+  const [serverMessages, setServerMessages] = useState<string[]>([]);
+  const [connectionStatus, setConnectionStatus] = useState<ConnectionStatus>({status: "Disconnected"});
+  const [inputMessage, setInputMessage] = useState<string>("");
+  const [userName, setUserName] = useState<string>("");
+
+
+  const ws = usePartySocket({
+    host: "localhost:1999",
+    room: "tic-tac-toe",
+    id: userName,
+
+    onOpen() {
+      console.log("Connected!")
+      setConnectionStatus({status: "Connected", userName: userName})
+    },
+    onMessage(message) {
+      console.log("Message:", message.data)
+      // if message data is json and can be aligned to ChatMessage, then add to chatMessages
+      // else add to serverMessages
+      try {
+        const chatMessage: ChatMessage = {time: new Date(Date.now()), ...JSON.parse(message.data)};
+        setChatMessages([...chatMessages, chatMessage]);
+      } catch (e) {
+        setServerMessages([message.data, ...serverMessages]);
+      }
+    },
+    onClose() {
+      console.log("Disconnected!")
+      setConnectionStatus({status: "Disconnected"})
+    },
+    onError(error) {
+      console.log("Error:", error)
+      setConnectionStatus({status: `Error`})
+    }
+  });
+
+
+  const handleInputMessageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputMessage(event.target.value);
+  }
+
+  const handleMessageSend = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (inputMessage) {
+      ws.send(inputMessage);
+      // clear input from input field
+      setInputMessage("");
+    } else {
+      alert("No message to send!");
+    }
+  }
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+
+      <h2 className="text-lg font-bold mb-4">Messages:</h2>
+      <div className="bg-white rounded-lg shadow-lg p-4 h-36 w-96 overflow-y-scroll">
+        <div className="space-y-2">
+          <DisplayServerMessages messages={serverMessages} />
+        </div>
+      </div>
+      {/* Divider */}
+      <div className="border-t-2 border-teal-200 my-2 w-96"></div>
+
+      <div className="bg-white rounded-lg shadow-lg p-4 h-96 w-96 overflow-y-scroll">
+        <div className="space-y-2">
+          <DisplayMessages messages={chatMessages} />
+          <div id="bottom-of-messages" style={{ float:"left", clear: "both" }} /> {/* this is a hack to make the div scroll to the bottom */}
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <form className="w-full flex max-w-sm" onSubmit={handleMessageSend}>
+      <ShowConnectionStatus status={connectionStatus} />
+        <div className="flex items-center border-b border-blue-500 py-2">
+          <input className="appearance-none bg-transparent border-none w-full text-white-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="What's on your mind?" value={inputMessage} onChange={handleInputMessageChange}/>
+          <button className="flex-shrink-0 bg-blue-500 hover:bg-teal-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+            Send
+          </button>
+        </div>
+      </form>
     </main>
   )
 }
